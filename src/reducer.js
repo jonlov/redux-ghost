61d7882a165d1a1cc6dd5646fae052789c1f8612
Jonathan Lovera <jonathan@rootplace.com>
2019-05-28 10:30:58 +1000

    Added pages and settings new actions to reducer and initial state

--- src/reducer.js
+++ src/reducer.js
@@ -2,28 +2,35 @@ import {
   GET_POSTS,
   GET_POST,
   GET_POST_SLUG,
+  GET_PAGES,
+  GET_PAGE,
+  GET_PAGE_SLUG,
   GET_TAGS,
   GET_TAG,
   GET_TAG_SLUG,
   GET_USERS,
   GET_USER,
   GET_USER_SLUG,
-  RESET,
-} from './action-types';
+  GET_SETTINGS,
+  RESET
+} from "./action-types";
 
 const initialData = { data: null, error: null, loading: false, meta: null };
 
 const initialState = {
   posts: initialData,
   post: initialData,
+  pages: initialData,
+  page: initialData,
   tags: initialData,
   tag: initialData,
   users: initialData,
   user: initialData,
+  settings: initialData
 };
 
-const createStateHandler = (state, action) => (key) => {
-  const isSingle = !(key.substr(key.length - 1) === 's');
+const createStateHandler = (state, action) => key => {
+  const isSingle = !(key.substr(key.length - 1) === "s");
 
   const statusHandlers = {
     error: () => ({
@@ -31,53 +38,61 @@ const createStateHandler = (state, action) => (key) => {
       [key]: {
         ...state[key],
         data: null,
-        error: action.error || 'Unknown Error',
-        loading: false,
-      },
+        error: action.error || "Unknown Error",
+        loading: false
+      }
     }),
     loading: () => ({
       ...state,
       [key]: {
         ...state[key],
         loading: true,
-        error: null,
-      },
+        error: null
+      }
     }),
     success: () => ({
       ...state,
       [key]: {
         ...state[key],
-        data: isSingle ? action.data[isSingle ? `${key}s` : key][0] : action.data[isSingle ? `${key}s` : key],
+        data: isSingle
+          ? action.data[isSingle ? `${key}s` : key][0]
+          : action.data[isSingle ? `${key}s` : key],
         meta: action.data.meta || null,
         error: null,
-        loading: false,
-      },
-    }),
+        loading: false
+      }
+    })
   };
 
-  return statusHandlers[action.status] ? statusHandlers[action.status]() : state;
+  return statusHandlers[action.status]
+    ? statusHandlers[action.status]()
+    : state;
 };
 
 const reducer = (state, action) => {
-  if (typeof state === 'undefined') {
+  if (typeof state === "undefined") {
     return initialState;
   }
 
   const updateKey = createStateHandler(state, action);
 
   const reducers = {
-    [GET_POSTS]: () => updateKey('posts'),
-    [GET_POST]: () => updateKey('post'),
-    [GET_POST_SLUG]: () => updateKey('post'),
-    [GET_TAGS]: () => updateKey('tags'),
-    [GET_TAG]: () => updateKey('tag'),
-    [GET_TAG_SLUG]: () => updateKey('tag'),
-    [GET_USERS]: () => updateKey('users'),
-    [GET_USER]: () => updateKey('user'),
-    [GET_USER_SLUG]: () => updateKey('user'),
+    [GET_POSTS]: () => updateKey("posts"),
+    [GET_POST]: () => updateKey("post"),
+    [GET_POST_SLUG]: () => updateKey("post"),
+    [GET_PAGES]: () => updateKey("pages"),
+    [GET_PAGE]: () => updateKey("page"),
+    [GET_PAGE_SLUG]: () => updateKey("page"),
+    [GET_TAGS]: () => updateKey("tags"),
+    [GET_TAG]: () => updateKey("tag"),
+    [GET_TAG_SLUG]: () => updateKey("tag"),
+    [GET_USERS]: () => updateKey("users"),
+    [GET_USER]: () => updateKey("user"),
+    [GET_USER_SLUG]: () => updateKey("user"),
+    [GET_SETTINGS]: () => updateKey("settings"),
     [RESET]: () => ({
-      ...initialState,
-    }),
+      ...initialState
+    })
   };
 
   return reducers[action.type] ? reducers[action.type]() : state;
